2c3be48aaad1da9b0c7bf5ef7a5fefa8b287a8dc
Neil Cherry <ncherry@linuxha.com>
2016-11-29 20:15:56 -0500

    Argh, I was misisng the new keyword for Etherio()

--- tests/test.js
+++ tests/test.js
@@ -7,23 +7,20 @@ var assert  = require('assert'),
 // off
 // ping
 describe('Etherio tests', function(){
-    it('should not fail', function(done){
-	var eio = Etherio('127.0.0.1', 2424); // It's UDP & we're not expecting a response
-	assert(eio);
-	var z = {};
-	z.port = "A";		// We don't need the rest here
-	z.pin  = 7;
+    var eio = new Etherio('127.0.0.1', 2424); // It's UDP & we're not expecting a response
+    var z  = {};
+    z.port = "A";		// We don't need the rest here
+    z.pin  = 7;
 
-	assert.strictEqual(eio.on(z), undefined);
+    it('Constructor test', function(done){
+	assert(eio);
+
 	done();
     });
 
-/*
-  it('should subtract', function(done){
-    assert.strictEqual(math.subtract(1,1), 0);
-    assert.strictEqual(math.subtract(10,5), 5);
-    assert.strictEqual(math.subtract(1,-1), 2);
-    done();
-  });
-*/
+    it('on function test', function(done){
+	assert(eio.on(z));
+
+	done();
+    });
 });
